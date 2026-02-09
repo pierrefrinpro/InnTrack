d71cc64af5ad228b6aecaf5e8d0cf68d76ff447e
pierrefrinpro <pierre.frin.pro@gmail.com>
2026-02-10 00:33:28 +0100

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import client, devis, demande, travaux
-
-admin.site.register(client)
-admin.site.register(devis)
-admin.site.register(demande)
-admin.site.register(travaux)
